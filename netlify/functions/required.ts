796e2d363b238b37d55648dac69ad6e925dd38d6
terence1988 <awstest135@gmail.com>
2022-07-06 13:10:27 +1000

    check keys

--- netlify/functions/required.ts
+++ netlify/functions/required.ts
@@ -1,10 +1,11 @@
 import { Handler } from "@netlify/functions";
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
+import {ManagementClient} from '@kentico/kontent-management'
 
 const handler: Handler = async (event, context) => {
- const data = await fetch(`https://google.com`);
- const text = await data.json();
- console.log(JSON.stringify(text,null,4));
+  const projectId = context?.clientContext?.projectId;
+  console.log(projectId);
+  //const client = new ManagementClient({});  // Create a new instance of the client
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World" }),
@@ -16,5 +17,8 @@ export { handler };
 // Endpoint
 // https://site-name.netlify.app/functions/required
 
-//- Removing your Function's "package.json" and adding the dependencies to the project's top-level "package.json" instead. 
+//- Removing your Function's "package.json" and adding the dependencies to the project's top-level "package.json" instead.
 //This is the fastest and safest solution.
+// Only run on node-fetch@2
+
+//rewrites key set value and property
