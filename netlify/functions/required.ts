6108bc9741149694b986f054eda1bd889c2b1bdd
terence1988 <awstest135@gmail.com>
2022-07-06 11:31:27 +1000

    dependency?

--- netlify/functions/required.ts
+++ netlify/functions/required.ts
@@ -1,6 +1,10 @@
 import { Handler } from "@netlify/functions";
+const fetch = require('node-fetch');
 
 const handler: Handler = async (event, context) => {
+ const data = await fetch(`https://google.com`);
+ const text = await data.json();
+ console.log(JSON.stringify(text,null,4));
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World" }),
